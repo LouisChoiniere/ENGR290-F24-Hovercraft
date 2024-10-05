89eca02d63a14fa572c57bf5314a3337a14f83e1
Louis Choiniere <louischoiniere@outlook.com>
2024-10-05 14:55:46 -0400

    make IMU work

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,30 @@
 #include <HardwareSerial.h>
 #include <Wire.h>
 
-#include "util/ADC.h"
 #include "sensors/GP2Y0A21YK.h"
 #include "sensors/MPU6050.h"
+#include "util/ADC.h"
 
 #define GP2Y0A21YK_ADC_CHANNEL 0
 
-MPU6050_Values_Struct MPU6050_Positions;
+MPU6050_Struct IMU;
 
 int pos = 0;
 
-void mySetup() {
+void setup() {
+  Serial.begin(115200); // Initialize serial communication
+  while (!Serial)
+    _delay_ms(10);
+  Serial.println("Start of program");
+  Serial.flush();
 
   ADC_setup();
-  // Wire.begin(); // Start the I2C communication
 
-  // MPU6050_WakeUpMPU(); // Send wake bit to IMU
+  Wire.begin(); // Start the I2C communication
+
+  MPU6050_WakeUpMPU(); // Send wake bit to IMU
+  MPU6050_SetAccelerationSensitivity();
+  MPU6050_SetGyroSensitivity();
 
 #pragma region timer0
 
@@ -52,32 +60,52 @@ void mySetup() {
   // OCR1B = 0;   // Output unsued
 
 #pragma endregion timer1
-}
-
-void myLoop() {
-
-  double dist = GP2Y0A21YK_GetDistance(GP2Y0A21YK_ADC_CHANNEL);
-
-  Serial.print("Distance: ");
-  Serial.println(dist, 2);
-  Serial.flush();
-
-  _delay_ms(100);
-}
-
-int main(void) {
-  Serial.begin(115200); // Initialize serial communication
-  while (!Serial)
-    _delay_ms(10);
-  Serial.println("Start of program");
-  Serial.flush();
-
-  mySetup();
 
   Serial.println("Start of Loop");
   Serial.flush();
-
-  while (1) {
-    myLoop();
-  }
 }
+
+void loop() {
+
+  double dist = GP2Y0A21YK_GetDistance(GP2Y0A21YK_ADC_CHANNEL);
+
+  MPU6050_ReadAcceleromterData(&IMU);
+  MPU6050_ReadGyroscopeData(&IMU);
+
+  Serial.print(" Accel_X: ");
+  Serial.print(IMU.acceleration_X);
+  Serial.print(" Accel_Y: ");
+  Serial.print(IMU.acceleration_Y);
+  Serial.print(" Accel_Z: ");
+  Serial.print(IMU.acceleration_Z);
+
+  Serial.print(" Gyro_X: ");
+  Serial.print(IMU.gyroscope_X);
+  Serial.print(" Gyro_Y: ");
+  Serial.print(IMU.gyroscope_Y);
+  Serial.print(" Gyro_Z: ");
+  Serial.print(IMU.gyroscope_Z);
+
+  Serial.print(" Distance: ");
+  Serial.println(dist, 2);
+  Serial.flush();
+
+  _delay_ms(500);
+}
+
+// int main(void) {
+//   Serial.begin(115200); // Initialize serial communication
+//   while (!Serial)
+//     _delay_ms(10);
+//   Serial.println("Start of program");
+//   Serial.flush();
+
+//   mySetup();
+
+//   Serial.println("Start of Loop");
+//   Serial.flush();
+
+//   while (1) {
+//     myLoop();
+//   }
+// }
--- src/sensors/MPU6050.h
+++ src/sensors/MPU6050.h
@@ -1,8 +1,8 @@
 #ifndef MPU6050_H
 #define MPU6050_H
 
-#include <avr/io.h>
 #include <Wire.h>
+#include <avr/io.h>
 
 #define MPU6050_ADDR 0x68
 
@@ -18,7 +18,7 @@ typedef struct {
   int16_t gyroscope_X;
   int16_t gyroscope_Y;
   int16_t gyroscope_Z;
-} MPU6050_Values_Struct;
+} MPU6050_Struct;
 
 void MPU6050_WakeUpMPU() {
   Wire.beginTransmission(MPU6050_ADDR);
@@ -27,36 +27,50 @@ void MPU6050_WakeUpMPU() {
   Wire.endTransmission(true);
 };
 
-void MPU6050_SetAccelerationRange();
-void MPU6050_SetGyroRange();
+void MPU6050_SetAccelerationSensitivity() {
+  Wire.beginTransmission(MPU6050_ADDR);
+  Wire.write(0x1C); // Talk to the ACCEL_CONFIG register (1C hex)
+  Wire.write(0x10); // Set the register bits as 00010000 (+/- 8g full scale range)
+  Wire.endTransmission(true);
+};
+
+void MPU6050_SetGyroSensitivity() {
+  Wire.beginTransmission(MPU6050_ADDR);
+  Wire.write(0x1B); // Talk to the GYRO_CONFIG register (1B hex)
+  Wire.write(0x10); // Set the register bits as 00010000 (1000deg/s full scale)
+  Wire.endTransmission(true);
+};
+
 void MPU6050_setFilterBandwidth();
 
-void MPU6050_ReadAcceleromterData(MPU6050_Values_Struct* MPU6050) {
+
+// Returns m/s^2 ??
+void MPU6050_ReadAcceleromterData(MPU6050_Struct *MPU6050) {
   Wire.beginTransmission(MPU6050_ADDR);
   Wire.write(0x3B); // 0x3B -> ACCEL_XOUT_H
   Wire.endTransmission(false);
   Wire.requestFrom(MPU6050_ADDR, 6, true);
-  
-  //For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
+
+  // For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
   MPU6050->acceleration_X = (Wire.read() << 8 | Wire.read()) / 16384.0;
   MPU6050->acceleration_Y = (Wire.read() << 8 | Wire.read()) / 16384.0;
   MPU6050->acceleration_Z = (Wire.read() << 8 | Wire.read()) / 16384.0;
   Wire.endTransmission(true);
 };
 
-void MPU6050_ReadGyroscopeData(MPU6050_Values_Struct* MPU6050){
+// Returns rad/s ??
+void MPU6050_ReadGyroscopeData(MPU6050_Struct *MPU6050) {
   Wire.beginTransmission(MPU6050_ADDR);
   Wire.write(0x43); // 0x43 -> Gyro data
   Wire.endTransmission(false);
   Wire.requestFrom(MPU6050_ADDR, 6, true);
-  
+
   // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
   MPU6050->gyroscope_X = (Wire.read() << 8 | Wire.read()) / 131.0;
   MPU6050->gyroscope_Y = (Wire.read() << 8 | Wire.read()) / 131.0;
   MPU6050->gyroscope_Z = (Wire.read() << 8 | Wire.read()) / 131.0;
   Wire.endTransmission(true);
 };
-
 
 // void MPU6050_Read
 
