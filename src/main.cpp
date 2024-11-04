94ab37b070d93ec200fb5072aa30bd2276c46444
Louis Choiniere <louischoiniere@outlook.com>
2024-11-03 22:21:46 -0500

    Work on IMU

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@ void setup() {
   Serial.begin(115200); // Initialize serial communication
   while (!Serial)
     _delay_ms(10);
+  
   Serial.println("Start of program");
   Serial.flush();
 
@@ -33,10 +34,14 @@ void setup() {
   MPU6050_WakeUpMPU(); // Send wake bit to IMU
   MPU6050_SetAccelerationSensitivity(&IMU, MPU6050_ACCELEROMETER_RANGE_4G);
   MPU6050_SetGyroSensitivity(&IMU, MPU6050_GYROSCOPE_RANGE_500);
+  MPU6050_SetFilter(MPU6050_FILTER_BW_20);
 
+  MPU6050_Calibrate(&IMU, 5000);
 
   Serial.println("Start of Loop");
   Serial.flush();
+
+  _delay_ms(100);
 }
 
 void loop() {
@@ -47,50 +52,54 @@ void loop() {
 
   MPU6050_ALL(&IMU);
 
-  if (counter % 1000 == 0) {
-    Serial.print(" Accel_X: ");
+  if (counter % 100 == 0) {
+    Serial.print("Accel (X, Y, Z): (");
     Serial.print(IMU.acceleration_X);
-    Serial.print(" Accel_Y: ");
+    Serial.print(", ");
     Serial.print(IMU.acceleration_Y);
-    Serial.print(" Accel_Z: ");
+    Serial.print(",");
     Serial.print(IMU.acceleration_Z);
+    Serial.print(")");
 
-    Serial.print(" Gyro_X: ");
+    Serial.print("\tGyro (X, Y, Z): (");
     Serial.print(IMU.gyroscope_X);
-    Serial.print(" Gyro_Y: ");
+    Serial.print(", ");
     Serial.print(IMU.gyroscope_Y);
-    Serial.print(" Gyro_Z: ");
+    Serial.print(", ");
     Serial.print(IMU.gyroscope_Z);
+    Serial.print(")");
     
-    Serial.print(" Velocity_X: ");
+    Serial.print("\tVelocity (X, Y, Z): (");
     Serial.print(IMU.velocity_X);
-    Serial.print(" Velocity_Y: ");
+    Serial.print(", ");
     Serial.print(IMU.velocity_Y);
-    Serial.print(" Velocity_Z: ");
+    Serial.print(", ");
     Serial.print(IMU.velocity_Z);
+    Serial.print(")");
 
-    Serial.print(" Position_X: ");
+    Serial.print("\tPosition (X, Y, Z): (");
     Serial.print(IMU.position_X);
-    Serial.print(" Position_Y: ");
+    Serial.print(", ");
     Serial.print(IMU.position_Y);
-    Serial.print(" Position_Z: ");
+    Serial.print(", ");
     Serial.print(IMU.position_Z);
+    Serial.print(")");
 
-    Serial.print(" Pitch: ");
-    Serial.print(IMU.pitch);
-    Serial.print(" Roll: ");
-    Serial.print(IMU.roll);
-    Serial.print(" Yaw: ");
-    Serial.print(IMU.yaw);
+    // Serial.print(" Pitch: ");
+    // Serial.print(IMU.pitch);
+    // Serial.print(" Roll: ");
+    // Serial.print(IMU.roll);
+    // Serial.print(" Yaw: ");
+    // Serial.print(IMU.yaw);
 
 
 
-    Serial.print(" Distance: ");
+    Serial.print("\tDistance: ");
     Serial.println(dist, 2);
     Serial.flush();
   }
 
-  _delay_ms(10);
+  _delay_ms(5);
 }
 
 // int main(void) {
--- src/sensors/MPU6050.h
+++ src/sensors/MPU6050.h
@@ -19,6 +19,14 @@
 #define MPU6050_GYROSCOPE_RANGE_1000 0x10
 #define MPU6050_GYROSCOPE_RANGE_2000 0x18
 
+#define MPU6050_FILTER_BW_260 0x00
+#define MPU6050_FILTER_BW_184 0x01
+#define MPU6050_FILTER_BW_94 0x02
+#define MPU6050_FILTER_BW_42 0x03
+#define MPU6050_FILTER_BW_20 0x04
+#define MPU6050_FILTER_BW_10 0x05
+#define MPU6050_FILTER_BW_5 0x06
+
 #define ACCEL_DIV_2G 16384
 #define ACCEL_DIV_4G 8192
 #define ACCEL_DIV_8G 4096
@@ -29,32 +37,43 @@
 #define GYRO_DIV_1000 33
 #define GYRO_DIV_2000 16
 
+#define GRAVITY 9.81
+
 typedef struct {
   uint16_t acceleration_division_factor;
   uint16_t gyroscope_division_factor;
 
-  int16_t acceleration_X;
-  int16_t acceleration_Y;
-  int16_t acceleration_Z;
+  float calibration_acceleraton_X;
+  float calibration_acceleraton_Y;
+  float calibration_acceleraton_Z;
 
-  int16_t gyroscope_X;
-  int16_t gyroscope_Y;
-  int16_t gyroscope_Z;
+  float calibration_gyroscope_X;
+  float calibration_gyroscope_Y;
+  float calibration_gyroscope_Z;
 
-  uint16_t velocity_X;
-  uint16_t velocity_Y;
-  uint16_t velocity_Z;
+  // Acceleration in m/s^2
+  float acceleration_X;
+  float acceleration_Y;
+  float acceleration_Z;
 
-  uint16_t position_X;
-  uint16_t position_Y;
-  uint16_t position_Z;
+  // Angular velocity in rad/s
+  float gyroscope_X;
+  float gyroscope_Y;
+  float gyroscope_Z;
 
-  double roll;
-  double pitch;
-  double yaw;
+  float velocity_X = 0;
+  float velocity_Y = 0;
+  float velocity_Z = 0;
+
+  float position_X = 0;
+  float position_Y = 0;
+  float position_Z = 0;
+
+  double roll = 0;
+  double pitch = 0;
+  double yaw = 0;
 
   uint64_t last_time;
-  uint64_t dt;
 } MPU6050_Struct;
 
 void MPU6050_WakeUpMPU() {
@@ -114,50 +133,99 @@ void MPU6050_SetGyroSensitivity(MPU6050_Struct *MPU6050, int sensitivity) {
   Wire.endTransmission(true);
 };
 
-// Returns m/s^2 ??
+void MPU6050_SetFilter(int filter) {
+  Wire.beginTransmission(MPU6050_ADDR);
+  Wire.write(MPU6050_FILTER_CONFIG_REGISTERS);
+  Wire.write(filter);
+  Wire.endTransmission(true);
+};
+
 void MPU6050_ReadAcceleromterData(MPU6050_Struct *MPU6050) {
   Wire.beginTransmission(MPU6050_ADDR);
-  Wire.write(0x3B); // 0x3B -> Acel data
+  Wire.write(0x3B);
   Wire.endTransmission(false);
   Wire.requestFrom(MPU6050_ADDR, 6, true);
 
-  MPU6050->acceleration_X = (Wire.read() << 8 | Wire.read()) / MPU6050->acceleration_division_factor;
-  MPU6050->acceleration_Y = (Wire.read() << 8 | Wire.read()) / MPU6050->acceleration_division_factor;
-  MPU6050->acceleration_Z = (Wire.read() << 8 | Wire.read()) / MPU6050->acceleration_division_factor;
+  int16_t raw_accel_X = (Wire.read() << 8 | Wire.read());
+  int16_t raw_accel_Y = (Wire.read() << 8 | Wire.read());
+  int16_t raw_accel_Z = (Wire.read() << 8 | Wire.read());
+
+  uint16_t division_factor = MPU6050->acceleration_division_factor;
+  MPU6050->acceleration_X = ((float)raw_accel_X / division_factor) * 9.8 - MPU6050->calibration_acceleraton_X;
+  MPU6050->acceleration_Y = ((float)raw_accel_Y / division_factor) * 9.8 - MPU6050->calibration_acceleraton_Y;
+  MPU6050->acceleration_Z = ((float)raw_accel_Z / division_factor) * 9.8 - MPU6050->calibration_acceleraton_Z;
+
   Wire.endTransmission(true);
 };
 
-// Returns rad/s ??
 void MPU6050_ReadGyroscopeData(MPU6050_Struct *MPU6050) {
-
   Wire.beginTransmission(MPU6050_ADDR);
-  Wire.write(0x43); // 0x43 -> Gyro data
+  Wire.write(0x43);
   Wire.endTransmission(false);
   Wire.requestFrom(MPU6050_ADDR, 6, true);
 
-  MPU6050->gyroscope_X = (Wire.read() << 8 | Wire.read()) / MPU6050->gyroscope_division_factor;
-  MPU6050->gyroscope_Y = (Wire.read() << 8 | Wire.read()) / MPU6050->gyroscope_division_factor;
-  MPU6050->gyroscope_Z = (Wire.read() << 8 | Wire.read()) / MPU6050->gyroscope_division_factor;
+  uint16_t raw_gyro_X = (Wire.read() << 8 | Wire.read());
+  uint16_t raw_gyro_Y = (Wire.read() << 8 | Wire.read());
+  uint16_t raw_gyro_Z = (Wire.read() << 8 | Wire.read());
+
+  uint16_t division_factor = MPU6050->gyroscope_division_factor;
+  MPU6050->gyroscope_X = ((float)raw_gyro_X / division_factor) - MPU6050->calibration_gyroscope_X;
+  MPU6050->gyroscope_Y = ((float)raw_gyro_Y / division_factor) - MPU6050->calibration_gyroscope_Y;
+  MPU6050->gyroscope_Z = ((float)raw_gyro_Z / division_factor) - MPU6050->calibration_gyroscope_Z;
+
   Wire.endTransmission(true);
 };
+
+void MPU6050_Calibrate(MPU6050_Struct *MPU6050, uint32_t time) {
+  float sum_accel_X = 0, sum_accel_Y = 0, sum_accel_Z = 0;
+  float sum_gyro_X = 0, sum_gyro_Y = 0, sum_gyro_Z = 0;
+  uint32_t samples = 0;
+  uint32_t start_time = millis();
+
+  while (millis() - start_time < time) {
+    MPU6050_ReadAcceleromterData(MPU6050);
+    MPU6050_ReadGyroscopeData(MPU6050);
+
+    sum_accel_X += MPU6050->acceleration_X;
+    sum_accel_Y += MPU6050->acceleration_Y;
+    sum_accel_Z += MPU6050->acceleration_Z;
+
+    sum_gyro_X += MPU6050->gyroscope_X;
+    sum_gyro_Y += MPU6050->gyroscope_Y;
+    sum_gyro_Z += MPU6050->gyroscope_Z;
+
+    samples++;
+    delay(10); // Small delay to allow sensor to stabilize
+  }
+
+  MPU6050->calibration_acceleraton_X = sum_accel_X / samples;
+  MPU6050->calibration_acceleraton_Y = sum_accel_Y / samples;
+  MPU6050->calibration_acceleraton_Z = (sum_accel_Z / samples) - GRAVITY;
+
+  MPU6050->calibration_gyroscope_X = sum_gyro_X / samples;
+  MPU6050->calibration_gyroscope_Y = sum_gyro_Y / samples;
+  MPU6050->calibration_gyroscope_Z = sum_gyro_Z / samples;
+}
 
 void MPU6050_ALL(MPU6050_Struct *MPU6050) {
   MPU6050_ReadAcceleromterData(MPU6050);
   MPU6050_ReadGyroscopeData(MPU6050);
 
   uint64_t current_time = millis();
-  MPU6050->dt = current_time - MPU6050->last_time;
+  uint16_t dt = current_time - MPU6050->last_time;
+  float dt_seconds = dt / 1000.0;
+  MPU6050->last_time = current_time;
 
-  MPU6050->velocity_X = MPU6050->acceleration_X * MPU6050->dt;
-  MPU6050->velocity_Y = MPU6050->acceleration_Y * MPU6050->dt;
-  MPU6050->velocity_Z = MPU6050->acceleration_Z * MPU6050->dt;
+  MPU6050->velocity_X += MPU6050->acceleration_X * dt_seconds;
+  MPU6050->velocity_Y += MPU6050->acceleration_Y * dt_seconds;
+  MPU6050->velocity_Z += (MPU6050->acceleration_Z - GRAVITY) * dt_seconds;
 
-  MPU6050->position_X += MPU6050->velocity_X * MPU6050->dt;
-  MPU6050->position_Y += MPU6050->velocity_Y * MPU6050->dt;
-  MPU6050->position_Z += MPU6050->velocity_Z * MPU6050->dt;
+  MPU6050->position_X += MPU6050->velocity_X * dt_seconds;
+  MPU6050->position_Y += MPU6050->velocity_Y * dt_seconds;
+  MPU6050->position_Z += MPU6050->velocity_Z * dt_seconds;
 
-  MPU6050->pitch = 0;
-  MPU6050->roll = 0;
-  MPU6050->yaw = 0;
+  // MPU6050->pitch = 0;
+  // MPU6050->roll = 0;
+  // MPU6050->yaw = 0;
+
 }
-
