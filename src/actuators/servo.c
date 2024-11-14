11265a8b83758b2e091b9e65400583c462e665e3
Louis Choiniere <louischoiniere@outlook.com>
2024-11-14 17:34:02 -0500

    fix servo v1

--- src/actuators/servo.c
+++ src/actuators/servo.c
@@ -1,13 +1,13 @@
 #include "servo.h"
 
 void SERVO_setPosition(int8_t angle) {
-    if (angle > 45) {
-        angle = 45;
-    } else if (angle < -45) {
-        angle = -45;
+    if (angle > 90) {
+        angle = 90;
+    } else if (angle < -90) {
+        angle = -90;
     }
 
-    double ticks = (double)187.5 + angle * (double)1.11;
+    double ticks = (double)187.5 + ((double)angle * (double)1.35);
 
     OCR1A = (int)ticks;
 }
--- src/actuators/servo.h
+++ src/actuators/servo.h
@@ -6,6 +6,11 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Sets position of the servo in degress.
+ *
+ * @param angle Desired angle from -90 degress to +90 degress.
+ */
 void SERVO_setPosition(int8_t angle);
 
 #ifdef __cplusplus
