ff967ec49e64b0e73b9e59ddec94af05c737f742
Louis Choiniere <louischoiniere@outlook.com>
2024-11-29 09:45:23 -0500

    Demo completed the maze successfully

--- src/config.h
+++ src/config.h
@@ -5,7 +5,7 @@
 #define ENABLE_FANS 1
 #define ENABLE_SERVO 1
 
-#define LIFT_FAN_SPEED 93
+#define LIFT_FAN_SPEED 92
 #define LIFT_FAN_SPEED_SLOW 80
 #define THRUST_FAN_SPEED_HIGH 85
 #define THRUST_FAN_SPEED 65
@@ -14,9 +14,9 @@
 
 #define TURNING_DISTANCE_THRESHOLD_CM 40
 #define ANGLE_THRESHOLD_END_TURN 10
-#define TURNING_TIME_MS 1500
+#define TURNING_TIME_MS 1700
 
-#define P_GAIN_SERVO 0.8
+#define P_GAIN_SERVO 0.85
 
 // ----- Pinout selections -----
 #define IR_LEFT 0           // P5
